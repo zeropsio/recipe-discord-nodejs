10c647e023ad2d98f1fcc7643f2301dbf757068c
Rahhhh <meow@nermalcat69.dev>
2024-07-15 20:00:10 +0530

    chore: fix syntax err

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,39 @@
-import { Client } from "discord.js";
+import { Client, CommandInteraction, GatewayIntentBits, Interaction } from "discord.js";
 import { config } from "./config";
 import { commands } from "./commands";
 import { deployCommands } from "./deploy-commands";
 
 const client = new Client({
-  intents: ["Guilds", "GuildMessages", "DirectMessages"],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
 });
 
 client.once("ready", () => {
   console.log("Discord bot is ready! 🤖");
 
   client.user?.setActivity({
-    name: " on zerops.io"
-  })
-    client.user?.setStatus("online")
+    name: "on zerops.io"
+  });
+  client.user?.setStatus("online");
 });
 
-client.on("guildCreate", async (guild: { id: any; }) => {
+client.on("guildCreate", async (guild) => {
   await deployCommands({ guildId: guild.id });
 });
 
-
-client.on("interactionCreate", async (interaction: { isCommand?: any; commandName?: any; }) => {
+client.on("interactionCreate", async (interaction: Interaction) => {
   if (!interaction.isCommand()) {
     return;
   }
-  const { commandName } = interaction;
+
+  const commandInteraction = interaction as CommandInteraction;
+
+  const { commandName } = commandInteraction;
   if (commands[commandName as keyof typeof commands]) {
-    commands[commandName as keyof typeof commands].execute(interaction);
+    commands[commandName as keyof typeof commands].execute(commandInteraction);
   }
 });
 
